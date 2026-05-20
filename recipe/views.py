6f938e2df6b5773807d8e9e34d977b17ee44dae7
Kira Mikhoparova <mihoparovakira@gmail.com>
2026-05-20 14:10:54 +0300

    Add view

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render
+from .models import Recipe
 
+def main(request):
+    recipes = Recipe.objects.order_by('?')[:10]
+    return render(request, 'main.html', {'recipes': recipes})
