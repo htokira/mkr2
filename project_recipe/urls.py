85c2a83510c2a8de4991e099cfb741d9bf88587e
Kira Mikhoparova <mihoparovakira@gmail.com>
2026-05-20 14:21:48 +0300

    Fix urls

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name='main'),
+    path('', include('recipe.urls')),
 ]
